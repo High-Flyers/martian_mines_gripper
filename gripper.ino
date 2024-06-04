0e7fa7b80740610edd13cd36ad6ae65a73184fd1
Kacper <ks300578@student.polsl.pl>
2024-06-04 13:04:36 +0200

    gradient

--- gripper.ino
+++ gripper.ino
@@ -16,12 +16,21 @@
 #define serv 9
 
 #define LED_PIN 14
-
+#define MAXIMUM_BRIGHTNESS 255
 Servo s;
 
 ACS712  ACS(A7, 5.0, 1023, 100);
 
 Adafruit_NeoPixel strip(12, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+int maxBrightness = 255;
+int minBrightness = 0;
+
+uint16_t hsvb = 25000;
+auto colorb = Adafruit_NeoPixel::ColorHSV(hsvb, 255, 255);
+
+uint16_t hsvr = 55000;
+auto colorr = Adafruit_NeoPixel::ColorHSV(hsvr, 255, 255);
 
 void setup() {
   Serial.begin(9600);
@@ -68,9 +77,10 @@ void loop() {
   Serial.println(digitalRead(kranc_L));
   Serial.println(digitalRead(kranc_R));
   Serial.println(digitalRead(kranc_B));
-  colorWipe(strip.Color(  0,   0, 255), 100, 3, 8); // Blue
-  colorWipe(strip.Color(  255, 0, 0), 100, 0, 2); // Red
-  colorWipe(strip.Color(  255, 0, 0), 100, 9, 11);
+
+  redgrad();
+  bluegrad();
+
   delay(100);
 }
 
@@ -81,3 +91,49 @@ void colorWipe(uint32_t color, int wait, int start, int end) {
     delay(wait);                           //  Pause for a moment
   }
 }
+
+void redgrad(){
+  
+  hsvr = hsvr + 1000;
+  if(hsvr > 10000)
+    hsvr = 55000;
+  colorr = Adafruit_NeoPixel::ColorHSV(hsvr, 255, 255);
+  colorWipe(colorr, 100, 0, 2); // Red
+  colorWipe(colorr, 100, 9, 11);
+  if(hsvr >= 65000){
+    hsvr = 0;
+  }
+}
+
+void bluegrad(){
+  hsvb = hsvb + 1000;
+  if(hsvb > 45000){
+    hsvb = 25000;
+  }
+  colorb = Adafruit_NeoPixel::ColorHSV(hsvb, 255, 255);
+  colorWipe(colorb, 100, 3, 8); // Blue
+}
+
+/*
+void ring(){
+
+  strip.setPixelColor(0, colorr1);
+  strip.setPixelColor(1, colorr2);
+  strip.setPixelColor(2, colorr3);
+
+  strip.setPixelColor(3, colorb1);
+  strip.setPixelColor(4, colorb2);
+  strip.setPixelColor(5, colorb3);
+
+  strip.setPixelColor(6, colorb1);
+  strip.setPixelColor(7, colorb2);
+  strip.setPixelColor(8, colorb3);
+
+  strip.setPixelColor(9, colorr1);
+  strip.setPixelColor(10, colorr2);
+  strip.setPixelColor(11, colorr3);
+
+  strip.show(); 
+
+}
+*/
